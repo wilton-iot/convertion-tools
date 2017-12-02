a2c99a8678caf5c366bfdf7cf226cee06323b73d
alex <alex@staticlibs.net>
2017-12-02 18:38:44 +0000

    convert script minor enhancement

--- node-module-convert.js
+++ node-module-convert.js
@@ -30,7 +30,8 @@ function convert(modname, path) {
         output.write("\n");
     }
     while(null !== line) {
-        line = line.replace(new RegExp("(require\\(\\'|\\\")\\.\\.?\\/", "g"), "\$1" + modname + "/");
+        line = line.replace(new RegExp("(require\\((:?\\'|\\\"))\\.\\.?\\/", "g"), "\$1" + modname + "/");
+        line = line.replace(new RegExp("(require\\((:?\\'|\\\")[a-zA-Z0-9\\-]+)\\/(\\'|\\\")", "g"), "\$1\$2");
         output.write(line);
         output.write("\n");
         line = input.readLine();
